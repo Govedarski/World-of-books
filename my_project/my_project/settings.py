19fa594107314e67fffb4bd93388ef7d967708c3
Govedarski <velislavgovedarski@gmail.com>
2022-04-02 17:23:50 +0300

    Debug==True to see the error

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3&ul(x3&ze^(!f3ze3+f@=-hf7j(z1=_2a8d&&*-)avtudy5d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
