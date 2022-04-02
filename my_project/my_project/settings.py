14ca0dfd904339bec0abab948e9cdeeff1dff9ef
Govedarski <velislavgovedarski@gmail.com>
2022-04-02 15:29:23 +0300

    Setup staticfiles

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -158,10 +158,14 @@ USE_L10N = False
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 MEDIA_URL = 'media/'
 
 MEDIA_ROOT = BASE_DIR / 'media'
