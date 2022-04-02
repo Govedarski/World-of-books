b24e5d59a1acd422d4f09647fa6a55769b799b97
Govedarski <velislavgovedarski@gmail.com>
2022-04-02 16:24:45 +0300

    Setup datebase for Heroku

--- my_project/my_project/settings.py
+++ my_project/my_project/settings.py
@@ -162,7 +162,7 @@ USE_L10N = False
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
